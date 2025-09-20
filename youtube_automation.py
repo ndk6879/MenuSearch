34cd4a38dc40fe8ad867b3a1c990d46a82141a10
Donggyun Nam <ndk6879@naver.com>
2025-09-20 19:19:54 -0400

    add menudata

--- youtube_automation.py
+++ youtube_automation.py
@@ -278,7 +278,7 @@ def ask_sonar_from_comment(comment_text, source_name=""):
 
 # ✅ 실행 부분
 videos_all = get_video_ids_and_channel(API_KEY, CHANNEL_ID, max_results=200)
-videos = videos_all[:10]
+videos = videos_all[:50]
 existing_urls = get_existing_urls()
 youtube = build("youtube", "v3", developerKey=API_KEY)
 initialize_js_file_if_needed()
